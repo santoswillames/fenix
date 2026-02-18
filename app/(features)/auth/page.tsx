345ee31fdc3b122b42a805dd67089039622d3fe4
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2026-02-18 09:40:27 -0300

    chore: configure eslint with prettier and format project files

--- app/(features)/auth/page.tsx
+++ app/(features)/auth/page.tsx
@@ -1,3 +1,3 @@
 export default function Auth() {
-  return <div>Auth</div>;
+  return <div>Auth</div>
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,23 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
 
-export default eslintConfig;
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+
+  prettierConfig, // Desativa regras que conflitam com Prettier
+
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+])
