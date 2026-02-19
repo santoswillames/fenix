1cea97362c533ef18432ae9b7d2981662e46cdc9
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2026-02-19 09:28:59 -0300

    feat: create reusable card component for tickets

--- app/(features)/tickets/page.tsx
+++ app/(features)/tickets/page.tsx
@@ -1,3 +1,48 @@
+import { Header } from '@/components/header'
+import { StatsCard } from '@/components/stats-card'
+import { Button } from '@/components/ui/button'
+import {
+  CircleCheck,
+  CircleCheckBig,
+  Clock,
+  MessageCircleReply,
+  Plus,
+  Ticket,
+} from 'lucide-react'
+
 export default function TicketsPage() {
-  return <div>Tickets</div>
+  return (
+    <div>
+      <Header title="Tickets">
+        <Button className="rounded-full shadow-[0_0_10px_0_#1876D2] hover:shadow-[0_0_20px_2px_#1876D2] transition-all duration-300">
+          <Plus className="mr-2" size={16} />
+          Novo Ticket
+        </Button>
+      </Header>
+      <main className="pl-30 p-10 flex flex-col gap-10">
+        <div className="flex gap-6 w-full">
+          <StatsCard
+            title="Tickets Abertos"
+            data={15}
+            icon={<Ticket size={28} color="#43D2CB" />}
+          />
+          <StatsCard
+            title="Em andamento"
+            data={8}
+            icon={<MessageCircleReply size={28} color="#D2B843" />}
+          />
+          <StatsCard
+            title="Resolvidos hoje"
+            data={12}
+            icon={<CircleCheckBig size={28} color="#43D2CB" />}
+          />
+          <StatsCard
+            title="Tempo médio"
+            data={15}
+            icon={<Clock size={28} color="#59BCDD" />}
+          />
+        </div>
+      </main>
+    </div>
+  )
 }
--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,15 @@
+import React from 'react'
+
 interface HeaderProps {
   title: string
+  children?: React.ReactNode
 }
 
-export function Header({ title }: HeaderProps) {
+export function Header({ title, children }: HeaderProps) {
   return (
     <div className="h-22 w-full font-semibold bg-secondary flex items-center justify-between pl-30 px-10">
       {title}
+      {children}
     </div>
   )
 }
